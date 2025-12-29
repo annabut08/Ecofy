8a7d6b9bb7b03c1f270baa8a69646593243ea25e
Anna But <ann28876@gmail.com>
2025-12-29 13:25:26 +0200

    Delete models

--- backend/src/schemas/doc.py
+++ backend/src/schemas/doc.py
@@ -19,23 +19,3 @@ class WasteTransferActDTO(BaseModel):
 
     transfer_datetime: datetime
     waste_description: str
-
-
-class RoutePointDTO(BaseModel):
-    container_site_id: int
-    address: str
-    scheduled_time: time
-    completed_time: Optional[time]
-
-
-class RouteSheetDTO(BaseModel):
-    route_date: date
-
-    organization_name: str
-    organization_edrpou: str
-    organization_phone: str
-
-    vehicle_name: str
-    vehicle_number_plate: str
-
-    route_points: List[RoutePointDTO]
