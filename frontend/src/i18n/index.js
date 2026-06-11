faf70b1e7b6f121d4d6c13733139e9078f07003b
NureButAnna <@email>
2026-06-12 01:15:31 +0300

    Refactor i18n configuration and clean up CSS styles

--- frontend/src/i18n/index.js
+++ frontend/src/i18n/index.js
@@ -1,8 +1,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import uk from "./locales/uk.json";
-import en from "./locales/en.json";
+import uk from "./locales/ukr.json";
+import en from "./locales/eng.json";
 
 i18n
   .use(LanguageDetector)
